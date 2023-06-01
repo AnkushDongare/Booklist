a3ac461e4797b86eae708456ab724d9dd5a05332
ANKUSH DONGRE <dongreankush52@gmail.com>
2023-06-01 23:32:46 +0530

    Complete Booklist App

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import App from './App'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>This is a boilerplate for react js</h1>
+    <App />
   </React.StrictMode>,
 )
---
+++ src/books.jsx
@@ -0,0 +1,39 @@
+import img1 from './images/img1.jpg'
+import img2 from './images/img2.jpg'
+import img3 from './images/img3.webp'
+import img4 from './images/img4.webp'
+import img5 from './images/img5.jpg'
+
+const books = [{
+        id: 1,
+        image: img1,
+        title: "Tomb of Sand",
+        author: "Geetanjali Shree"
+    },
+    {
+        id: 2,
+        image: img2,
+        title: "THE MAGIC",
+        author: "Rhonda Byrne"
+    },
+    {
+        id: 3,
+        image: img3,
+        title: "The Silent Patient",
+        author: "Alex Michaelides "
+    },
+    {
+        id: 4,
+        image: img4,
+        title: "Jungle Book",
+        author: "Rudyard Kipling"
+    },
+    {
+        id: 5,
+        image: img5,
+        title: "The Little Prince",
+        author: "Antoine de Saint-Exupery"
+    }
+]
+
+export default books
